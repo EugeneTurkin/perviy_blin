0e16ddd955518ef81e3e3652e1c922075542a346
Eugene Toorkin <amubeer@gmail.com>
2022-04-27 23:16:57 +0300

    first solution with sqlite

--- main.py
+++ main.py
@@ -1,28 +1,73 @@
 import os
-import password_manager
+import sqlite3
+from rot import rot13
+
+
+class User():
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
 
 
 def logging_in():
     login = input('Enter your login: ')
     password = input('Enter your password: ')
-    return f'{login},{password}'
+    return User(login, password)
 
 
-if os.path.exists(os.getcwd() + '\\users'):
+def manager(acc_info):
+    user_input = input('Press "1" to add password. Press "2" to retrieve your password: ')
+    if user_input == '1':
+        user_email = input('Enter your e-mail: ')
+        user_password = input('Enter your password: ')
+        curs.execute("INSERT INTO userdat (owner_id, email, password) VALUES (:owner_id, :email, :password)",
+        {'owner_id': curs.execute("SELECT id FROM accounts WHERE email=:email AND password=:password", {'email': acc_info.email, 'password': acc_info.password}).fetchone()[0],
+        'email': user_email, 'password': user_password})
+        print('Password is saved successfully')
+    elif user_input == '2':
+        user_email = input('Enter your e-mail: ')
+        user_password = curs.execute("SELECT password FROM userdat WHERE owner_id=:owner_id AND userdat.email=:email",
+        {'owner_id': curs.execute("SELECT id FROM accounts WHERE email=:email AND password=:password", {'email': acc_info.email, 'password': acc_info.password}).fetchone()[0],
+        'email': user_email}).fetchone()
+        user_password = user_password[0] if user_password is not None else ''
+        print(f'Your password is: {rot13(user_password)}')
+
+
+if os.path.exists(os.path.dirname(os.path.abspath(__file__)) + '\\users.db'):
+    conn = sqlite3.connect('users.db')
+    curs = conn.cursor()
     print('Welcome back!')
     acc_info = logging_in()
-    if acc_info not in os.listdir(os.getcwd() + '\\users'):
-        input('This account does not exist. Creating a new one.')
-        os.makedirs(f'users\\{acc_info}')
+    if curs.execute("SELECT email, password FROM accounts WHERE email=:email AND password=:password;", {'email': acc_info.email, 'password': acc_info.password}) is None:
+        print('This account does not exist. Creating a new one.')
+        curs.execute("INSERT INTO account (id, email, password) VALUES (:id, :email, :password)", {'id': curs.execute("SELECT (COUNT(*) + 1) FROM accounts;"), 'email': acc_info.email, 'password': acc_info.password})
 else:
     print("Welcome to Gill Bates' Password Manager! Please, create an account by following the instructions.")
     acc_info = logging_in()
-    os.makedirs(f'users\\{acc_info}')
+    conn = sqlite3.connect('users.db')
+    curs = conn.cursor()
+    curs.execute("""CREATE TABLE accounts (
+                id          INTEGER     NOT_NULL,
+                email       TEXT        NOT_NULL,
+                password    TEXT        NOT_NULL
+                )""")
+    curs.execute("INSERT INTO accounts (id, email, password) VALUES (:id, :email, :password)", {'id': curs.execute("SELECT (COUNT(*) + 1) FROM accounts;").fetchone()[0], 'email': acc_info.email, 'password': acc_info.password})
+    curs.execute("""CREATE TABLE userdat (
+                owner_id    INTEGER     NOT_NULL,
+                email       TEXT        NOT_NULL,
+                password    TEXT        NOT_NULL
+                )""")
+    conn.commit()
 
-os.chdir(f'users\\{acc_info}')
-password_manager.manager(acc_info)
+
+manager(acc_info)
+conn.close()
+
+
 
 
 # файлы должны браться *относительно* места, где они лежат?
 # хранение информации в БД
-
+# 1) вынести создание таблиц в отдельный файл("установка")
+# 2) гит (гитхаб ето другое) (системыа контроля версий)
+# 3) 
